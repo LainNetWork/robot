73b78ed0f1e76824864677a68a099c261074e128
Lain <tianshang360@163.com>
2020-11-02 02:34:44 +0800

    add:添加服务注册与发现，添加consul支持

--- src/main/java/fun/lain/robot/RobotApplication.java
+++ src/main/java/fun/lain/robot/RobotApplication.java
@@ -8,11 +8,13 @@ import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @EnableCaching
+@EnableDiscoveryClient
 @EnableScheduling
 public class RobotApplication {
 
