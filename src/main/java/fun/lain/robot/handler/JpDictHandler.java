75044a28b87af92410fc2272d4a98e1421f2040d
Lain <tianshang360@163.com>
2020-11-04 12:06:27 +0800

    add: 添加日语词典功能

---
+++ src/main/java/fun/lain/robot/handler/JpDictHandler.java
@@ -0,0 +1,55 @@
+package fun.lain.robot.handler;
+
+import net.mamoe.mirai.message.MessageEvent;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+/**
+ * @author zhiang.liu <zhiang.liu@fujfu.com>
+ * @date 2020/11/4 10:42
+ */
+@Component
+public class JpDictHandler implements MessageHandler{
+    private static final String URL = "https://www.weblio.jp/content/%s";
+    private static final String SPLIT = "jp ";
+    @Override
+    public int order() {
+        return 0;
+    }
+
+    @Override
+    public String name() {
+        return "jpDict";
+    }
+
+    @Override
+    public String describe() {
+        return "日语词典";
+    }
+
+    @Override
+    public void handleMsg(MessageEvent contact) throws Exception {
+        String content = contact.getMessage().contentToString();
+        String jp = content.substring(SPLIT.length());
+        Connection connect = Jsoup.connect(String.format(URL, jp));
+        Document document = connect.get();
+        Elements kijiWrp = document.select(".kijiWrp");
+        Optional<Element> first = kijiWrp.stream().findFirst();
+        if(first.isPresent()){
+            contact.getSubject().sendMessage(first.get().text());
+        }else {
+            contact.getSubject().sendMessage("没有查到哦");
+        }
+    }
+
+    @Override
+    public boolean isMatch(MessageEvent msg) {
+        return getFirstPlainTextMsg(msg).startsWith("jp ");
+    }
+}
